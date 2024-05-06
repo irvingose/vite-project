e59bae2c5521c2eadc759bec31fa7ff90c298f2c
Franco Mitrushi <franco.9930@gmail.com>
2024-05-06 16:41:31 +0200

    feat: addedd handleLogin feature

--- src/MyForm.jsx
+++ src/MyForm.jsx
@@ -24,6 +24,13 @@ export function MyForm() {
       };
     });
   }
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    console.log("Logging in with:", data.username, data.password);
+
+    setData(createData);
+  };
 
   function handleResetForm() {
     setData(createData);
@@ -32,25 +39,28 @@ export function MyForm() {
   return (
     <div>
       <h1>My Form</h1>
-      <input
-        name="username"
-        value={data.username}
-        onChange={handleInputChange}
-      />
-      <input
-        name="password"
-        type="password"
-        value={data.password}
-        onChange={handleInputChange}
-      />
-      <input
-        name="session"
-        type="checkbox"
-        checked={data.session}
-        onChange={handleInputChange}
-      />
-      <button disabled={!data.username || !data.password}>Login</button>
-      <button onClick={handleResetForm}>Reset</button>
+      <form onSubmit={handleLogin}>
+        <input
+          name="username"
+          value={data.username}
+          onChange={handleInputChange}
+        />
+        <input
+          name="password"
+          type="password"
+          value={data.password}
+          onChange={handleInputChange}
+        />
+        <input
+          name="session"
+          type="checkbox"
+          checked={data.session}
+          onChange={handleInputChange}
+        />
+
+        <button disabled={!data.username || !data.password}>Login</button>
+        <button onClick={handleResetForm}>Reset</button>
+      </form>
 
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
